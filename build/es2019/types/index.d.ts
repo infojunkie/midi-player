52035891053033a496daedf22d3bf2d39d5d8269
infojunkie <karim.ratib@gmail.com>
2024-05-13 19:32:36 -0700

    Remove unneeded files

--- build/es2019/types/index.d.ts
+++ build/es2019/types/index.d.ts
@@ -2,6 +2,4 @@ export * from './midi-file-slicer-factory';
 export * from './midi-player-factory';
 export * from './midi-player-factory-factory';
 export * from './player-state';
-export * from './midi-controller-message';
-export * from './midi-registered-parameter-number';
 //# sourceMappingURL=index.d.ts.map
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,3 @@ export * from './midi-file-slicer-factory';
 export * from './midi-player-factory';
 export * from './midi-player-factory-factory';
 export * from './player-state';
-export * from './midi-controller-message';
-export * from './midi-registered-parameter-number';
